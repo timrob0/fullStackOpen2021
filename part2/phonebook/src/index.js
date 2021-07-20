b90f7eab716636d349bc652572718ab5f6db805b
Timothy Robinson <timothy.robinson123@gmail.com>
2021-07-20 13:49:31 +1000

    Completed first questions

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -7,19 +7,50 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const addName  = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const Person = ({person}) => {
+    return (
+      <div>
+        <p>{person.name}</p>
+      </div>
+    )
+  }
+
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => 
+          <Person key={person.name} person={person} />
+        )}
     </div>
   )
 }
