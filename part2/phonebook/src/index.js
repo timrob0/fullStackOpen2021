84ba1ff973394bb72bccd7bf371cc71e9dd027a7
Timothy Robinson <timothy.robinson123@gmail.com>
2021-07-24 13:52:40 +1000

    Created filter search

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,64 +1,86 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterPerson, setFilterPerson] = useState("");
+
+  const personToShow = persons.find((person) => person.name === filterPerson)
+    ? persons.filter((person) => person.name === filterPerson)
+    : persons;
 
   const addName = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const nameObject = {
       name: newName,
-    }
+      number: newNumber,
+    };
 
-    if (persons.find(person => person.name === newName)) {
-      alert("This use already exists");
-
+    if (persons.find((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons(persons.concat(nameObject))
+      setPersons(persons.concat(nameObject));
     }
-    setNewName('')
-  }
+    setNewName("");
+    setNewNumber("");
+  };
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
 
-  const Person = ({person}) => {
+  const handleFilterChange = (event) => {
+    setFilterPerson(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const Person = ({ person }) => {
     return (
       <div>
-        <p>{person.name}</p>
+        <p>
+          {person.name} {person.number}
+        </p>
       </div>
-    )
-  }
-
+    );
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with
+      <input value={filterPerson} onChange={handleFilterChange} />
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name:
-          <input
-            value={newName}
-            onChange={handleNameChange}
-          />
+          <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number:
+          <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
-          <Person key={person.name} person={person} />
-        )}
+      {personToShow.map((person) => (
+        <Person key={person.name} person={person} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 ReactDOM.render(<App />, document.getElementById("root"));
