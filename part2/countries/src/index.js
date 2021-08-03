6b826e6c36095ae8151d4feebe3e2eb24d3e0490
Timothy Robinson <timothy.robinson123@gmail.com>
2021-08-03 14:18:27 +1000

    Completed the countries part, need to do weather now

--- part2/countries/src/index.js
+++ part2/countries/src/index.js
@@ -1,17 +1,98 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import { uuid } from "uuidv4";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filterCountry, setFilterCountry] = useState("");
+  const [masterCountry, setMasterCountry] = useState([]);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const hook = () => {
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      setMasterCountry(response.data);
+    });
+  };
+
+  useEffect(hook, []);
+
+  const handleFilterChange = (event) => {
+    setFilterCountry(event.target.value);
+
+    if (filterCountry) {
+      const countShow = masterCountry.filter((country) =>
+        country.name.toLowerCase().match(filterCountry.toLowerCase())
+      )
+        ? masterCountry.filter((country) =>
+            country.name.toLowerCase().match(filterCountry.toLocaleLowerCase())
+          )
+        : masterCountry;
+
+      setCountries(countShow);
+    }
+  };
+
+  const CountryShow = (props) => {
+    if (countries.length > 10) {
+      return <div>Too many countries to list</div>;
+    }
+
+    if (countries.length === 1) {
+      return (
+        <div>
+          <CountrySingle country={countries[0]} />
+        </div>
+      );
+    }
+    return (
+      <>
+        {countries.map((country) => (
+          <Country key={uuid()} country={country} />
+        ))}
+      </>
+    );
+  };
+
+  const CountrySingle = (props) => {
+    return (
+      <>
+        <h2>{props.country.name}</h2>
+        <p>capital {props.country.capital}</p>
+        <p>population {props.country.population}</p>
+        <h3>languages</h3>
+        <ul>
+          {props.country.languages.map((language) => (
+            <li key={language.name}>{language.name}</li>
+          ))}
+        </ul>
+        <img
+          src={props.country.flag}
+          alt="Flag of country"
+          width="150"
+          height="150"
+        />
+      </>
+    );
+  };
+
+  const Country = (props) => {
+    return (
+      <div>
+        {props.country.name}
+        <button onClick={() => setCountries([props.country])}>show</button>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      find countries
+      <input value={filterCountry} onChange={handleFilterChange} />
+      <CountryShow />
+    </div>
+  );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
